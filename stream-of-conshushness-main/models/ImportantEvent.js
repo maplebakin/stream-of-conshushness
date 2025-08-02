46ed84edb669361aefe8701320aaab7aea970b7e
MadiChristine <madison.alway@gmail.com>
2025-08-02 18:16:07 -0400

    chore: prune duplicate repo & node_modules; update .gitignore

--- stream-of-conshushness-main/models/ImportantEvent.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const importantEventSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  title: { type: String, required: true },
-  date: { type: String, required: true }
-}, { timestamps: true });
-
-export default mongoose.model('ImportantEvent', importantEventSchema);
--- stream-of-conshushness-main/models/Note.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const noteSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  date: { type: String, required: true },
-  content: { type: String, default: '' }
-}, { timestamps: true });
-
-export default mongoose.model('Note', noteSchema);
